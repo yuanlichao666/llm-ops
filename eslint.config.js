133ab13f8e42412addd5e25395fa08316f979dda
ylc <yuanlichao666@163.com>
2025-11-11 10:45:38 +0800

    feat(project): ✨  setup project

---
+++ eslint.config.js
@@ -0,0 +1,73 @@
+const eslint = require("@eslint/js");
+const { defineConfig } = require("eslint/config");
+const globals = require("globals");
+const tseslint = require("typescript-eslint");
+const eslintPrettier = require("eslint-plugin-prettier");
+const importSort = require("eslint-plugin-simple-import-sort");
+const reactHooks = require("eslint-plugin-react-hooks");
+const reactRefresh = require("eslint-plugin-react-refresh");
+
+const ignores = [
+  "dist",
+  "build",
+  "**/*.js",
+  "**/*.mjs",
+  "**/*.d.ts",
+  "eslint.config.js",
+  "commitlint.config.js",
+];
+
+const frontendConfig = {
+  files: ["apps/frontend/**/*.{ts,tsx}"],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globales: globals.browser,
+  },
+  plugins: {
+    "react-hooks": reactHooks,
+    "react-refresh": reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "no-console": "error",
+  },
+};
+
+const backendConfig = {
+  files: ["apps/backend/**/*.ts"],
+  languageOptions: {
+    globals: {
+      ...globals.node, // global
+      ...globals.jest,
+    },
+    parser: tseslint.parser,
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "no-console": "error",
+  },
+};
+
+module.exports = defineConfig(
+  {
+    ignores,
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+    plugins: {
+      prettier: eslintPrettier,
+      "simple-import-sort": importSort,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "simple-import-sort/imports": "error",
+    },
+  },
+  frontendConfig,
+  backendConfig
+);
