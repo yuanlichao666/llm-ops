6cfc727069678c56965a138f2eeced8def36dfb3
ylc <yuanlichao666@163.com>
2026-01-07 10:49:27 +0800

    feat(frontend): ✨  add next-start

---
+++ apps/frontend/nextjs-start/app/layout.tsx
@@ -0,0 +1,19 @@
+import '@/app/ui/global.css'
+
+import clsx from 'clsx'
+
+import { inter } from '@/app/ui/fonts'
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body className={clsx(inter.className, 'antialiased')}>
+                {children}
+            </body>
+        </html>
+    )
+}
--- eslint.config.js
+++ eslint.config.js
@@ -8,57 +8,66 @@ const reactHooks = require('eslint-plugin-react-hooks')
 const reactRefresh = require('eslint-plugin-react-refresh')
 const pluginNext = require('@next/eslint-plugin-next')
 
-const ignores = ['**/.next/**', 'dist', 'build', '**/*.js', '**/*.mjs', '**/*.d.ts', 'eslint.config.js', 'commitlint.config.js']
+const ignores = [
+    '**/.next/**',
+    'dist',
+    'build',
+    '**/*.js',
+    '**/*.mjs',
+    '**/*.d.ts',
+    'eslint.config.js',
+    'commitlint.config.js',
+]
 
 const frontendConfig = {
-  files: ['apps/frontend/**/*.{ts,tsx}'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-  },
-  plugins: {
-    '@next/next': pluginNext,
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    'no-console': 'error',
-  },
+    files: ['apps/frontend/**/*.{ts,tsx}'],
+    languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+    },
+    plugins: {
+        '@next/next': pluginNext,
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+    },
+    rules: {
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': 'off',
+        'no-console': 'error',
+    },
 }
 
 const backendConfig = {
-  files: ['apps/backend/**/*.ts'],
-  languageOptions: {
-    globals: {
-      ...globals.node, // global
-      ...globals.jest,
+    files: ['apps/backend/**/*.ts'],
+    languageOptions: {
+        globals: {
+            ...globals.node, // global
+            ...globals.jest,
+        },
+        parser: tseslint.parser,
     },
-    parser: tseslint.parser,
-  },
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'no-console': 'error',
-  },
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-console': 'error',
+    },
 }
 
 module.exports = defineConfig(
-  globalIgnores(ignores),
-  {
-    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
-    plugins: {
-      prettier: eslintPrettier,
-      'simple-import-sort': importSort,
+    globalIgnores(ignores),
+    {
+        extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+        plugins: {
+            prettier: eslintPrettier,
+            'simple-import-sort': importSort,
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            'simple-import-sort/imports': 'error',
+        },
     },
-    rules: {
-      'prettier/prettier': 'error',
-      'simple-import-sort/imports': 'error',
-    },
-  },
-  frontendConfig,
-  backendConfig
+    frontendConfig,
+    backendConfig
 )
